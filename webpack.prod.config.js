0f8db968376644abdb563aef0b7f0c9fb6e294e3
qiaoduan <chunbo.ycb@alibaba-inc.com>
2016-06-16 19:08:14 +0800

    deploy

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -13,7 +13,7 @@ module.exports = {
         path: path.resolve(__dirname, 'build'),
         filename: '[hash].[name].bundle.js',
         chunkFilename: '[hash].[id].bundle.js',
-        publicPath: '/fprmui/build/'
+        publicPath: '/fprmui/fp/build/'
     },
     module: {
         loaders: [
