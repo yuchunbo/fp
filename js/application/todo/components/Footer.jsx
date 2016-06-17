bfa218d44d4eb228d206607cc507fdd200611727
qiaoduan <chunbo.ycb@alibaba-inc.com>
2016-06-17 18:56:08 +0800

    rollback

--- js/application/todo/components/Footer.jsx
+++ js/application/todo/components/Footer.jsx
@@ -13,13 +13,11 @@ class Footer extends React.Component {
             display: 'block',
             width: '100%',
             textAlign: 'center',
-            marginTop: '15px',
-            fontFamily:'华文行楷',
-            fontSize:'30px'
+            marginTop: '15px'
         };
         return (
             <footer style={ footerStyle }>
-              <span>{ '宠辱不惊 闲看庭前花开花落 haha' }</span><br/><span>{ '去留无意 漫随天外云卷云舒'}</span>
+              <span>{ '于洛舒' }</span>
             </footer>
             );
     }
