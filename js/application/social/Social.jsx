769421e6ff5887a21424d19434a9bbfc992a7d94
桥段 <yucubor@163.com>
2016-07-12 00:02:36 +0800

    social

--- js/application/social/Social.jsx
+++ js/application/social/Social.jsx
@@ -150,7 +150,7 @@ class Social extends React.Component {
                     <List>{socialList}</List>
                 </Panel>
                 <Footer
-				  initialSelectedIndex="1"
+				  value="social"
 				/>
             </div>
         );
